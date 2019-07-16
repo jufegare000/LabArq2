53a3a35650c79e3a8a05ee18b2eb4dfd4ccd9cd9
Jufegare000 <jufegare@gmail.com>
2019-07-16 18:04:18 -0500

    first commit

---
+++ lab2-1/lab2-1-ejb/src/main/java/com/udea/session/CustomerManager.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.session;
+
+import com.udea.model.Customer;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author juanf.gallo
+ */
+@Stateless
+public class CustomerManager implements CustomerManagerLocal {
+
+    @PersistenceContext(unitName = "com.udea_lab2-1-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+    
+    
+    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    @Override
+    public List<Customer> getAllCustomer() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+    
+    
+}
